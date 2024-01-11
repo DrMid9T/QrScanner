25a28fa438a89d801c71ed2315c0501beb61ba33
DrMid9T <codse211f013@student.nibm.lk>
2024-01-11 11:40:33 +0530

    update

---
+++ app/src/main/java/com/first/qrcodescanner/CaptureActivityPortrait.kt
@@ -0,0 +1,8 @@
+package com.first.qrcodescanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.journeyapps.barcodescanner.CaptureActivity
+
+class CaptureActivityPortrait : CaptureActivity() {
+}
